a1860a068f39bb4572f5eea5e2c6c6f0280400e5
Spartak Katvitskiy <katvitsn@mts.ru>
2024-07-06 21:02:59 +0300

    small fix

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,17 +1,16 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting() {
-     //   model.addAttribute("name", name);
-        return "greeting";
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "world") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting " + name;
     }
-
 }
