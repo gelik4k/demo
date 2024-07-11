2ae61e011e70192ca4e57639637041baabbcccdd
Spartak Katvitskiy <katvitsn@mts.ru>
2024-07-12 00:55:21 +0300

    fix package fix spring version

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,16 +1,45 @@
 package com.example.demo;
 
-import org.springframework.ui.Model;
+import com.example.demo.domain.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.repos.MessageRepo;
+
+import java.util.Map;
 
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "world") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting " + name;
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "world") String name,
+            Map<String, Object> model) {
+        model.put("name", name);
+        return "Hello " + name;
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
     }
 }
